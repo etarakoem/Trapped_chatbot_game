206ba024dffe0bb63522e5d53db5c0c5e73aa678
etarakoem <etarakoem@gmail.com>
2021-05-15 12:29:18 -0400

    add new 2 file

---
+++ mainGame.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class mainGame{
+    public static void main(String[] args){
+        messagePhrase intro = new messagePhrase();
+        // ArrayList <String> introduction = new intro.getMessages("Introduction");
+        intro.messagePrint("Introduction");
+    }
+}
